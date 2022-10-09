3be19f0749eb4c9c245daeb3c572c46296287990
DeboraCarmonaM <deboracarmonadani@gmail.com>
2022-10-09 18:20:42 -0300

    Rotas/navegação

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from "@angular/router";
+import { ContatoComponent } from "./institucional/contato/contato.component";
+import { SobreComponent } from "./institucional/sobre/sobre.component";
+import { HomeComponent } from "./navegacao/home/home.component";
+
+//constante com coleção de rotas
+export const rootRouterConfig: Routes = [  
+    { path: '', redirectTo: '/home', pathMatch: 'full'}, //path de redirecionamento para quando chamar vazio
+    { path: 'home', component: HomeComponent},
+    { path: 'contato', component: ContatoComponent},
+    { path: 'sobre', component: SobreComponent},
+
+];
